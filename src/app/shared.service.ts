0abe285dfe2f38c091719bf2df6108b6f416d3cf
Moh Kemal <mohakemal9@gmail.com>
2017-08-10 18:17:28 -0400

    shared file compiled

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -3,17 +3,11 @@ import { Http, Headers, Response } from "@angular/http";
 import 'rxjs/Rx';
 import { Observable } from "rxjs";
 
-
 @Injectable()
 export class SharedService {
-
-
    egenSolutionUrl = "http://mocker.egen.io/users/";
    allUsers = "bulk";
-
   public id : any;
-
-
   constructor(private _http: Http) { }
 
 public notifyOther(data: any) {
@@ -22,8 +16,6 @@ public notifyOther(data: any) {
         this.id = data.id;
       }
     }
-
-
   findAllUsers() {
                return this._http.get(this.egenSolutionUrl)
                .map(response => { { return response.json() }; })
@@ -41,12 +33,9 @@ public notifyOther(data: any) {
                         .map(response => { { return response }; })
                         .catch(error => Observable.throw(error.json().error));
                          }
-
    addUserToGroup(user) {
               return this._http.post(this.egenSolutionUrl,user)
                  .map(response => { { return response.json() }; })
                  .catch(error => Observable.throw(error.json().error));
                   }
-
-
 }
